29c320eea41478b3e6afccbee1fc9aceebfaf4dc
Leelavathi Gade <leelaroshangade@gmail.com>
2021-12-30 14:58:16 +0100

    redux-logger-added

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import allReducers from './reducers/index';
+import logger from 'redux-logger';
 
 
 const store = createStore(
   allReducers,
+  applyMiddleware(logger)
+  
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
